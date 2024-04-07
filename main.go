4f8634b757f00e3f5c127f6b032d75de7f2fd688
kcp <kcpetersen111@gmail.com>
2024-04-07 15:12:53 -0600

    bouncing gopher

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"flappyGopher/player"
 	"image"
 	"image/color"
 	_ "image/jpeg"
@@ -14,7 +15,7 @@ import (
 //go:embed src/gopher.jpg
 var gophFile []byte
 
-var gopherPic image.Image
+var goph *ebiten.Image
 
 func init() {
 	// put this outside of draw loop
@@ -28,16 +29,31 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	gopherPic = im
+
+	goph = ebiten.NewImageFromImage(im)
+
 }
 
 // Game implements ebiten.Game interface.
-type Game struct{}
+type Game struct {
+	player player.Player
+}
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
-	// Write your game's logical update.
+	if g.player.Direction {
+		g.player.Ypos += 3
+	} else {
+		g.player.Ypos -= 3
+	}
+
+	_, height := g.Layout(0, 0)
+
+	if g.player.Ypos+(0.5*float64(goph.Bounds().Size().Y)) >= float64(height) || g.player.Ypos <= 0 {
+		g.player.Direction = !g.player.Direction
+	}
+
 	return nil
 }
 
@@ -51,18 +67,27 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		G: 0,
 		A: 255,
 	})
-	goph := ebiten.NewImageFromImage(gopherPic)
-	screen.DrawImage(goph, nil)
+	op := ebiten.DrawImageOptions{}
+	//half image size, in real project dont scale image each draw
+	op.GeoM.Scale(0.5, 0.5)
+
+	op.GeoM.Translate(g.player.Xpos, g.player.Ypos)
+	screen.DrawImage(goph, &op)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 1024, 1024
 }
 
 func main() {
-	game := &Game{}
+	game := &Game{
+		player: player.Player{
+			Xpos: 100,
+			Ypos: 100,
+		},
+	}
 
 	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(640, 480)
---
+++ player/player.go
@@ -0,0 +1,7 @@
+package player
+
+type Player struct {
+	Xpos      float64
+	Ypos      float64
+	Direction bool
+}
