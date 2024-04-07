9c91a495393b8b5d64913e80a5a5e7abcc33bbad
kcp <kcpetersen111@gmail.com>
2024-04-07 15:46:19 -0600

    game ends and char no longer bounces around

--- main.go
+++ main.go
@@ -18,12 +18,6 @@ var gophFile []byte
 var goph *ebiten.Image
 
 func init() {
-	// put this outside of draw loop
-	// raw, err := os.ReadFile("flappyGopher/src/gopher.jpg")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	buf := bytes.NewBuffer(gophFile)
 	im, _, err := image.Decode(buf)
 	if err != nil {
@@ -34,19 +28,18 @@ func init() {
 
 }
 
-// Game implements ebiten.Game interface.
 type Game struct {
-	player player.Player
+	player  player.Player
+	running bool
 }
 
 // Update proceeds the game state.
-// Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
-	if g.player.Direction {
-		g.player.Ypos += 3
-	} else {
-		g.player.Ypos -= 3
+	if !g.running {
+		panic("Game over man")
 	}
+
+	g.player.Ypos += 3
 
 	spaceDown := ebiten.IsKeyPressed(ebiten.KeySpace)
 	if spaceDown {
@@ -56,7 +49,7 @@ func (g *Game) Update() error {
 	_, height := g.Layout(0, 0)
 
 	if g.player.Ypos+(0.5*float64(goph.Bounds().Size().Y)) >= float64(height) {
-		g.player.Direction = false
+		g.running = false
 	} else if g.player.Ypos <= 0 {
 		g.player.Direction = true
 	}
@@ -65,25 +58,24 @@ func (g *Game) Update() error {
 }
 
 // Draw draws the game screen.
-// Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Write your game's rendering.
+	// screen background
 	screen.Fill(color.RGBA{
 		R: 100,
 		B: 255,
 		G: 0,
 		A: 255,
 	})
+
 	op := ebiten.DrawImageOptions{}
 	//half image size, in real project dont scale image each draw
 	op.GeoM.Scale(0.5, 0.5)
-
 	op.GeoM.Translate(g.player.Xpos, g.player.Ypos)
+
+	//draw character
 	screen.DrawImage(goph, &op)
 }
 
-// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-// If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 1024, 1024
 }
@@ -94,13 +86,12 @@ func main() {
 			Xpos: 100,
 			Ypos: 100,
 		},
+		running: true,
 	}
 
-	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Flappy Gopher")
 
-	// Call ebiten.RunGame to start your game loop.
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
