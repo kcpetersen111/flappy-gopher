2896ab1ad7e6137515da0410da64a32ae662c182
kcp <kcpetersen111@gmail.com>
2024-04-07 12:45:40 -0600

    now will open image

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
+	"image"
+	"image/color"
+	_ "image/jpeg"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+//go:embed src/gopher.jpg
+var gophFile []byte
+
+var gopherPic image.Image
+
+func init() {
+	// put this outside of draw loop
+	// raw, err := os.ReadFile("flappyGopher/src/gopher.jpg")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	buf := bytes.NewBuffer(gophFile)
+	im, _, err := image.Decode(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gopherPic = im
+}
 
 // Game implements ebiten.Game interface.
 type Game struct{}
@@ -20,6 +45,14 @@ func (g *Game) Update() error {
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Write your game's rendering.
+	screen.Fill(color.RGBA{
+		R: 100,
+		B: 255,
+		G: 0,
+		A: 255,
+	})
+	goph := ebiten.NewImageFromImage(gopherPic)
+	screen.DrawImage(goph, nil)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
@@ -30,9 +63,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	game := &Game{}
+
 	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Your game's title")
+	ebiten.SetWindowTitle("Flappy Gopher")
+
 	// Call ebiten.RunGame to start your game loop.
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
